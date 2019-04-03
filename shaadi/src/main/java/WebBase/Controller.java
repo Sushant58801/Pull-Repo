51e94a0bbb199693f67c9f6a3c594365c6b6e3b1
S6670405 <S6670405@LT062360.zl.if.atcsg.net>
2019-04-03 13:32:12 +0530

    testing f=with GIT

--- shaadi/src/main/java/WebBase/Controller.java
+++ shaadi/src/main/java/WebBase/Controller.java
@@ -15,7 +15,7 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
-
+//test for good
 
 public class Controller {
 	
